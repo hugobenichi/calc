4d8ddd3d2c8a1058c6e3b2019b047835b279c947
Hugo Benichi <hugobenichi@gmail.com>
2018-03-09 23:11:02 +0900

    Converting custom stack to standard Vec

--- calc.rs
+++ calc.rs
@@ -20,49 +20,37 @@ fn parse(atom: &str) -> Atom {
   }
 }
 
-fn push_num(s: &mut [i64], c: &mut usize, a: i64) {
-  *c -= 1;
-  s[*c] = a;
-}
-
-fn pop_num(s: &mut [i64], c: &mut usize) -> i64 {
-  let a = s[*c];
-  *c += 1;
-  return a;
-}
-
-fn reduce(s: &mut [i64], c: &mut usize, a: Atom) {
+fn reduce(s: &mut Vec<i64>, a: Atom) {
   match a {
     Atom::Void    => {},
-    Atom::Num(x)  => push_num(s, c, x),
+    Atom::Num(x)  => s.push(x),
     Atom::Plus    => {
-      let y = pop_num(s, c);
-      let x = pop_num(s, c);
-      println!("after double pop, just before push, stack = {}:{:?}", c, s);
+      let y = s.pop().unwrap();
+      let x = s.pop().unwrap();
+      println!("after double pop, just before push, stack = {:?}", s);
       let z = x + y;
-      push_num(s, c, z);
+      s.push(z);
     }
     Atom::Minus   => {
-      let y = pop_num(s, c);
-      let x = pop_num(s, c);
+      let y = s.pop().unwrap();
+      let x = s.pop().unwrap();
       let z = x - y;
-      push_num(s, c, z);
+      s.push(z);
     }
   }
 }
 
 fn main() {
 
-  let mut stack : [i64; 10] = [0; 10];
-  let mut cursor : usize = 10;
+  let mut stack = Vec::new();
 
   for a in env::args() {
-    reduce(&mut stack, &mut cursor, parse(&a));
+    reduce(&mut stack, parse(&a));
 
-    println!("stack = {}:{:?}", cursor, stack);
+    println!("stack = {:?}", stack);
   }
 
-  for a in &stack[cursor..] {
+  for a in &stack {
     println!("{}", a);
   }
 }
