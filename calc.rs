f2390da20440069f5947a4da9ff7c9ad57afa213
Hugo Benichi <hugobenichi@gmail.com>
2018-03-09 18:42:06 +0900

    Sum numbers passed on command line args

--- calc.rs
+++ calc.rs
@@ -1,7 +1,21 @@
 use std::env;
 
 fn main() {
+
+  let mut ns = Vec::new();
+
   for a in env::args() {
     println!("{}", a);
+
+    match a.parse::<i64>() {
+      Ok(x) => ns.push(x),
+      _     => (),
+    }
   }
+
+  let mut t : i64 = 0;
+  for x in &ns {
+    t += x;
+  }
+  println!("{}", t);
 }
